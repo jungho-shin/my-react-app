75414667dd52592e9f03d37c43083d8525256a0a
jungho.shin <taos9938@gmail.com>
2026-01-16 11:05:44 +0900

    PointInTime, Direction 및 FileType 상수 객체 추가.

---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+// Point in time enum
+export const PointInTime = {
+  OLDEST: 'oldest',
+  LATEST: 'latest'
+};
+
+// Direction enum
+export const Direction = {
+  BEFORE: 'before',
+  AFTER: 'after'
+};
+
+// File type enum
+export const FileType = {
+  ALIGN: 'align',
+  LENS: 'lens',
+  LEVELING: 'leveling'
+};
+
